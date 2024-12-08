8268bf4f371a5239adf16f458538b47f36336117
oninp <ninoppagaran@gmail.com>
2024-12-09 05:18:51 +0800

    changed the color

--- src/gameHandler.cpp
+++ src/gameHandler.cpp
@@ -209,7 +209,7 @@ void gameHandler::updateGame()
         moveDownTimer = 0.0f;
     }
 
-    updateGhostBlock();
+    updateGhostBlock(); // Update ghost block position
 }
 
 void gameHandler::moveLeft()
