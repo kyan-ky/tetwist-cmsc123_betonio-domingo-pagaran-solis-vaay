2bb7d45901711d1055d29f86b1a287fdd9bcebdf
oninp <ninoppagaran@gmail.com>
2024-12-09 05:07:25 +0800

    feat(hold-once, long-press move): implements new features
    - Implements hold block only once
    - Implements long-press move/rotate block
    - Files changed has been saved with format C/C++ Formatter in VSCode

--- src/gameHandler.cpp
+++ src/gameHandler.cpp
@@ -33,6 +33,11 @@ gameHandler::gameHandler()
     canHoldPiece = true;
 
     updateGhostBlock(); // Initialize ghost block
+
+    moveDelay = 0.08f; // Delay in seconds (0.2 default)
+    lastMoveTime = 0.0f;
+
+    canHoldPiece = true;
 }
 
 gameHandler::~gameHandler()
